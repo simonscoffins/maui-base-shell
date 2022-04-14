474716cb37a5156aea2ad1aaf456068c7ac0a71c
Simon Scoffins <simonscoffins@gmail.com>
2022-04-14 09:47:07 -0400

    added mvvm helpers and created basic view models

---
+++ MauiBaseShell/ViewModels/PageOneViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBaseShell.ViewModels;
+
+public class PageOneViewModel : AppBaseViewModel {
+
+    public PageOneViewModel() {
+        Message = "Welcome to Page One!";
+    }
+}
+
---
+++ MauiBaseShell/ViewModels/PageTwoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBaseShell.ViewModels;
+
+public class PageTwoViewModel : AppBaseViewModel {
+
+    public PageTwoViewModel() {
+        Message = "Welcome to Page Two!";
+    }
+}
+
